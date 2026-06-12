05995aad5a1e18d1a7e69067d618c31545ed90dc
Gibranrghm <gibran.r.hdz@gmail.com>
2026-06-11 20:22:17 -0500

    Step 4: Add serializers for Product and Category

---
+++ store/serializers.py
@@ -0,0 +1,27 @@
+from rest_framework import serializers
+from .models import Category, Product
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['id', 'name', 'description', 'created_at']
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    category_name = serializers.ReadOnlyField(source='category.name')
+
+    class Meta:
+        model = Product
+        fields = [
+            'id',
+            'category',
+            'category_name',
+            'name',
+            'description',
+            'price',
+            'stock',
+            'available',
+            'created_at',
+            'updated_at',
+        ]
